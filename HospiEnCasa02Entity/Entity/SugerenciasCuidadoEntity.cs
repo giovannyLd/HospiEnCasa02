fccb83d2a91fb9ca3eec03e2f1d1d545950f768c
Sergio Arango <xhe13@hotmail.com>
2021-10-11 22:17:20 -0500

    prueba sergio

--- HospiEnCasa02Entity/Entity/SugerenciasCuidadoEntity.cs
+++ HospiEnCasa02Entity/Entity/SugerenciasCuidadoEntity.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace HospiEnCasa02Entity.Entity
 {
+    //prueba 2
     public class SugerenciasCuidadoEntity
     {
         public int IdSignosVitales { get; set; }
