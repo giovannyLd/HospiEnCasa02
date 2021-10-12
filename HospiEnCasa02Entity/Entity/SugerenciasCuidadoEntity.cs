7e7bb45e3def518d831008904fdf9705db005fb6
Sergio Arango <xhe13@hotmail.com>
2021-10-11 22:25:31 -0500

    correccion

--- HospiEnCasa02Entity/Entity/SugerenciasCuidadoEntity.cs
+++ HospiEnCasa02Entity/Entity/SugerenciasCuidadoEntity.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace HospiEnCasa02Entity.Entity
 {
-    //prueba 2
-    public class SugerenciasCuidadoEntity
+       public class SugerenciasCuidadoEntity
     {
         public int IdSignosVitales { get; set; }
         public DateTime? FechaHora { get; set; }
